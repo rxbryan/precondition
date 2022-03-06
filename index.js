a14d9ec81e08ee9d129293f7311faa7f408fb355
rxbryan <rxbryn@gmail.com>
2022-03-06 21:16:35 +0100

    init
    Node.js module for handling conditional requests
    (as defined in RFC 7232)

---
+++ index.js
@@ -0,0 +1,12 @@
+const HttpDate = require('http-date')
+
+module.exports = precondition
+
+/**
+ * @param {Object} 
+ * metadata: string|function that represents or generates last-modified or etag
+ * ignores last-modified if both are present
+ */
+function precondition (options) {
+
+}
