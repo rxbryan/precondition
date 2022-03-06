305153edf973c91b5cc283f4e9d6c7c3e1e24fa6
rxbryan <rxbryn@gmail.com>
2022-03-06 21:21:13 +0100

    add: etag comparison
    add funtions for comparing weak and strong etags

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ module.exports = precondition
 function precondition (options) {
 
 }
+
+const WEAK_ETAG_REGEX = /^W\//
+/**
+ * @param {String} etag/last-modified
+ */
+function weak_cmp(req_meta, res_meta) {
+  let match
+  if (WEAK_ETAG_REGEX.test(req_meta) || WEAK_ETAG_REGEX.test(res_meta)) {
+    req_meta = WEAK_ETAG_REGEX.test(req_meta) ? req_meta.substring(2) : req_meta
+    res_meta = WEAK_ETAG_REGEX.test(res_meta) ? res_meta.substring(2) : res_meta
+    match = (req_meta === res_meta)
+  } else {
+    match = (req_meta === res_meta)
+  }
+  return match
+}
+
+function strong_cmp(req_meta, res_meta) {
+  let match = 
+  if (WEAK_ETAG_REGEX.test(req_meta) || WEAK_ETAG_REGEX.test(res_meta)) {
+    match = false
+  } else {
+    match = (req_meta === res_meta)
+  }
+  return match
+}
+
