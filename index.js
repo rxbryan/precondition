a0148066c27d6ba189f996ab9e38feea680318db
rxbryan <rxbryn@gmail.com>
2022-03-11 21:05:55 +0100

    Inital release
    all tests passed with 100% coverage

--- index.js
+++ index.js
@@ -1,14 +1,68 @@
 const HttpDate = require('http-date')
 
-module.exports = precondition
+module.exports = Precondition
 
 /**
- * @param {Object} 
- * metadata: string|function that represents or generates last-modified or etag
- * ignores last-modified if both are present
+ * precondition headers in order of precedence as defined in
+ * rfc7232 section 6
  */
-function precondition (options) {
+const PRECONDITION_HEADERS = [
+  'if-match',
+  'if-unmodified-since',
+  'if-none-match',
+  'if-modified-since',
+  'if-range'
+]
+/**
+ * @param {Object} req
+ * @param {Object} options
+ * @param {Boolean} options.weak
+ * @param {String} options.etag
+ * @param {String} options.lastModified
+ * ignores options.lastModified if options.etag is present
+ */
+function Precondition (req, options) {
+  if (!(this instanceof Precondition))
+    return new Precondition(req, options)
+  let opts = options || {}
+  opts.weak = (typeof opts.weak === 'boolean')
+    ? opts.weak
+    : false
+  if (!opts.etag && !opts.lastModified)
+    throw new TypeError('Invalid resource metadata')
 
+  this.condition = false
+  for (field of PRECONDITION_HEADERS) {
+    if (field === 'if-modified-since' && req.headers[field]) {
+      if (req.method !== 'GET'  && req.method !== 'HEAD') {
+        continue
+      }
+    }
+    if (HttpDate.isValid(req.headers[field])) {
+      this.condition = date_cmp(req.headers, opts.lastModified)
+      break
+    } else {
+      let req_meta = req.headers[field] && req.headers[field].split(',')
+      if (!req_meta)
+        continue
+      for(elem of req_meta) {
+        let match = opts.weak
+          ? weak_cmp(elem, opts.etag)
+          : strong_cmp(elem, opts.etag)
+        switch(field) {
+          case 'if-match':
+          case 'if-range':
+            this.condition = match
+            break
+          case 'if-none-match':
+            this.condition = (!match)
+            break
+        }
+        if (match) break
+      }
+      break
+    }
+  }
 }
 
 const WEAK_ETAG_REGEX = /^W\//
@@ -16,8 +70,10 @@ const WEAK_ETAG_REGEX = /^W\//
  * @param {String} etag/last-modified
  */
 function weak_cmp(req_meta, res_meta) {
-  let match
-  if (WEAK_ETAG_REGEX.test(req_meta) || WEAK_ETAG_REGEX.test(res_meta)) {
+  let match = false
+  if (req_meta === '*')
+    match = true
+  else if (WEAK_ETAG_REGEX.test(req_meta) || WEAK_ETAG_REGEX.test(res_meta)) {
     req_meta = WEAK_ETAG_REGEX.test(req_meta) ? req_meta.substring(2) : req_meta
     res_meta = WEAK_ETAG_REGEX.test(res_meta) ? res_meta.substring(2) : res_meta
     match = (req_meta === res_meta)
@@ -28,45 +84,53 @@ function weak_cmp(req_meta, res_meta) {
 }
 
 function strong_cmp(req_meta, res_meta) {
-  let match = 
-  if (WEAK_ETAG_REGEX.test(req_meta) || WEAK_ETAG_REGEX.test(res_meta)) {
-    match = false
-  } else {
-    match = (req_meta === res_meta)
-  }
-  return match
+  return (req_meta === '*') ||
+    ((!WEAK_ETAG_REGEX.test(req_meta) && !WEAK_ETAG_REGEX.test(res_meta))
+    ? (req_meta === res_meta)
+    : false)
 }
 
+/**
+ * for strong comparison of last-modified metadata, the date header field
+ * should be atleast 60 seconds ahead of the last-modified time
+ */
 function date_cmp(req_meta, res_meta) {
-  let match
-  if (HttpDate.isValid(req_meta) && HttpDate.isValid(res_meta)) {
-    match = (new HttpDate(req_meta).getTime() < new HttpDate(res_meta).getTime()) 
-  } else {
-    match = false
+  if (!HttpDate.isValid(res_meta)){
+    return false
   }
-  return match
+  
+  let condition = false
+  if (req_meta['if-modified-since']) {
+    condition =
+      (new HttpDate(req_meta['if-modified-since']).getTime() < new HttpDate(res_meta).getTime())
+  } else if (req_meta['if-unmodified-since']) {
+    condition =
+      (new HttpDate(res_meta).getTime() <= new HttpDate(req_meta['if-unmodified-since']).getTime())
+  } 
+
+  if (req_meta['if-range']) {
+    console.log('range: ', req_meta['if-range'])
+    condition =
+      (new HttpDate(res_meta).getTime() <= new HttpDate(req_meta['if-range']).getTime())
+  }
+  return condition
 }
 
-function isConditional(req) {
+Precondition.isConditional =
+Precondition.prototype.isConditional = function isConditional(req) {
   let value = req.headers['if-match'] || req.headers['if-none-match'] ||
-    req.headers['if-modified-since'] || req.headers['if-unmodified-since'] || 
+    req.headers['if-modified-since'] || req.headers['if-unmodified-since'] ||
     req.headers['if-range']
 
     return Boolean(value)
 }
 
-function isRange(req) {
+Precondition.isRange =
+Precondition.prototype.isRange = function isRange(req) {
   return Boolean(req.headers['if-range'])
 }
 
-function isCacheRevalidation(req) {
+Precondition.isCacheRevalidation =
+Precondition.prototype.isCacheRevalidation = function isCacheRevalidation(req) {
   return Boolean(req.headers['if-modified-since'] || req.headers['if-none-match'])
 }
-
-function condition(argument) {
-  // body...
-}
-
-precondition.isRange = isRange
-precondition.isCacheRevalidation = isCacheRevalidation
-precondition.isConditional = isConditional
